e36129375fde08d401c4d20b96937aa85566579a
BatteryRip <turbochel1000@gmail.com>
2025-04-24 19:59:26 +0300

    db

---
+++ src/main/java/com/example/demo1/Configs.java
@@ -0,0 +1,9 @@
+package com.example.demo1;
+
+public class Configs {
+    protected String dbHost = "localhost";
+    protected String dbPort = "1433";
+    protected String dbUser = "qwerty";
+    protected String dbPass = "qwerty";
+    protected String dbName = "mash";
+}
---
+++ src/main/java/com/example/demo1/DatabaseHandler.java
@@ -0,0 +1,20 @@
+package com.example.demo1;
+
+import java.sql.*;
+
+public class DatabaseHandler extends Configs {
+    Connection dbConnection;
+    public Connection getConnection() throws ClassNotFoundException, SQLException {
+        String connectionUrl =
+                "jdbc:sqlserver://" + dbHost + ":" + dbPort + ";"
+                        + "database=" + dbName + ";"
+                        + "user=" + dbUser + ";"
+                        + "password=" + dbPass + ";"
+                        + "encrypt=true;"
+                        + "trustServerCertificate=false;"
+                        + "loginTimeout=30;encrypt=false;";
+        Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+        dbConnection = DriverManager.getConnection(connectionUrl);
+        return dbConnection;
+    }
+}
