c2560a90bf1d236adffb00bac94a88e974b89847
BatteryRip <turbochel1000@gmail.com>
2025-04-28 22:12:25 +0300

    вау оно авторизуется серьезно что-ли

--- src/main/java/com/example/demo1/AuthController.java
+++ src/main/java/com/example/demo1/AuthController.java
@@ -2,7 +2,14 @@ package com.example.demo1;
 
 import java.io.IOException;
 import java.net.URL;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ResourceBundle;
+
+import com.example.demo1.database.Accounts;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
@@ -12,6 +19,32 @@ import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
 public class AuthController {
+
+    DatabaseHandler dbHandler = new DatabaseHandler();
+    ObservableList<Accounts> list = FXCollections.observableArrayList();
+    public void authQuery(String email, String password) throws SQLException, ClassNotFoundException {
+        ResultSet res = null;
+        String query = "SELECT * FROM accounts WHERE email = '" + email + "' and password = '" + password + "'";
+        PreparedStatement ps = dbHandler.getConnection().prepareStatement(query);
+        res = ps.executeQuery();
+        list.removeAll(list);
+        while (res.next()) {
+            list.add(new Accounts(
+                    res.getString(1),
+                    res.getString(2),
+                    res.getString(3),
+                    res.getString(4)
+            ));
+        }
+    }
+
+    private void loginAccount(String login, String pass) {
+        if (!list.isEmpty()) {
+            Accounts account = list.get(0);
+            loginButton.getScene().getWindow().hide();
+            General.page("mainpage.fxml", 132, 247, "Пылесос умер");
+        }
+    }
 
     @FXML
     private ResourceBundle resources;
@@ -31,10 +64,18 @@ public class AuthController {
     @FXML
     void initialize() {
         loginButton.setOnAction(event -> {
-            String login = loginField.getText().trim();
-            String pass = passField.getText().trim();
-            loginButton.getScene().getWindow().hide();
-            General.page("mainpage.fxml", 132, 247, "Пылесос умер");
+            try {
+                authQuery(loginField.getText(), passField.getText());
+                loginAccount(loginField.getText(), passField.getText());
+            } catch (SQLException e) {
+                throw new RuntimeException(e);
+            } catch (ClassNotFoundException e) {
+                throw new RuntimeException(e);
+            }
+//            String login = loginField.getText().trim();
+//            String pass = passField.getText().trim();
+//            loginButton.getScene().getWindow().hide();
+//            General.page("mainpage.fxml", 132, 247, "Пылесос умер");
         });
 
     }
